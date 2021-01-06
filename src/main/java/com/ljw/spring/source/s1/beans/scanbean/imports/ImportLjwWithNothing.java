d8c5790faa9061b637068439aa632146374b12a1
lijinwu <1025462978@qq.com>
2021-01-06 17:30:42 +0800

    bean装配的集中方式

--- src/main/java/com/ljw/spring/source/s1/beans/scanbean/imports/ImportLjwWithNothing.java
+++ src/main/java/com/ljw/spring/source/s1/beans/scanbean/imports/ImportLjwWithNothing.java
@@ -1,5 +1,8 @@
 package com.ljw.spring.source.s1.beans.scanbean.imports;
 
+/**
+ * 被@Import注解注入类，最终会被组装BeanDefinition，并在单利创建的时候，进行初始化
+ */
 public class ImportLjwWithNothing {
 
     public ImportLjwWithNothing(){
