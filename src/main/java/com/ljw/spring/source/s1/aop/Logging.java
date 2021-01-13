c8938854394b34475a5dba7d5c0fe4a6bf76a0ea
lijinwu <1025462978@qq.com>
2021-01-13 22:26:22 +0800

    aop

--- src/main/java/com/ljw/spring/source/s1/aop/Logging.java
+++ src/main/java/com/ljw/spring/source/s1/aop/Logging.java
@@ -9,16 +9,32 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class Logging {
+    /**
+     * 将Pointcut和advice组合起来，
+     * 封装成一个对象，就是advisor（一个切面）
+     *
+     * 当spring扫描到@Aspect
+     * 就会去扫描Pointcut和@Around，就会封装成Advisor
+     */
 
 
-
-//    @Pointcut("execution()")
-    @Pointcut("within(com.ljw.spring.source.s1.service..*) ")
-    public void remoteCallPointcut() {
+    /**
+     * 这个是切点Pointcut
+     */
+    @Pointcut("execution(public * com.ljw.spring.source.s1.service.*.*(..))")
+//    @Pointcut("within(com.ljw.spring.source.s1.service..*) ")
+    public void pc1() {
     }
 
 
-    @Around("remoteCallPointcut()")
+    /**
+     * 这个就是advice
+     *
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around("pc1()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
 
         System.out.println("------around-before");
